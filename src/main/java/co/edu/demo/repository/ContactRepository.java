86bf265c5e1097abe6bf8f02ab49ddd2038b0a8b
Juan Pablo <JUGIRALDO1@GMAIL.COM>
2021-10-24 13:49:49 -0500

    Modificacion para obtener el json desde el jar

--- src/main/java/co/edu/demo/repository/ContactRepository.java
+++ src/main/java/co/edu/demo/repository/ContactRepository.java
@@ -3,45 +3,58 @@ package co.edu.demo.repository;
 import co.edu.demo.model.dto.ContactDTO;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.stereotype.Repository;
-import org.springframework.util.ResourceUtils;
 
-import java.io.File;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Repository;
+
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @Repository
-public class ContactRepository implements IContactRepository{
+public class ContactRepository implements IContactRepository {
 
     private List<ContactDTO> contacts;
 
-    public ContactRepository(){contacts =loadContactData();}
+    public ContactRepository() {
+        contacts = loadContactData();
+    }
+
+    public Resource loadEmployeesWithClassPathResource() {
+        return new ClassPathResource("contacts.json");
+    }
 
     private List<ContactDTO> loadContactData() {
-        File file =null;
-        try{
-            file = ResourceUtils.getFile("classpath:contacts.json");
+        InputStream dbAsStream = null;
+        try {
+            // file = ResourceUtils.getFile("classpath:contacts.json");
+            dbAsStream = loadEmployeesWithClassPathResource().getInputStream(); // <-- this is the difference
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        List<ContactDTO> contactDTOList =null;
-        try{
-            ObjectMapper objectMapper =new ObjectMapper();
-            TypeReference<List<ContactDTO>> typeRef = new TypeReference<>() {};
-            contactDTOList = objectMapper.readValue(file,typeRef);
-        }  catch (Exception e) {
+        List<ContactDTO> contactDTOList = null;
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            TypeReference<List<ContactDTO>> typeRef = new TypeReference<>() {
+            };
+            contactDTOList = objectMapper.readValue(dbAsStream, typeRef);
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return contactDTOList;
     }
 
     ;
+
     @Override
     public ContactDTO GetContact(Integer id) {
-        return contacts.stream().
-                filter( c -> c.getId().equals(id))
-                .findFirst().orElseThrow(()-> new RuntimeException());
+        return contacts.stream().filter(c -> c.getId().equals(id)).findFirst()
+                .orElseThrow(() -> new RuntimeException());
     }
 
     @Override
@@ -52,9 +65,8 @@ public class ContactRepository implements IContactRepository{
 
     @Override
     public ContactDTO UpdateContact(Integer id, ContactDTO contact) {
-        ContactDTO oldContact= contacts.stream().
-                filter( c -> c.getId().equals(id)).
-                findFirst().orElseThrow(()-> new RuntimeException());
+        ContactDTO oldContact = contacts.stream().filter(c -> c.getId().equals(id)).findFirst()
+                .orElseThrow(() -> new RuntimeException());
         contacts.remove(oldContact);
         contacts.add(contact);
         return contact;
@@ -62,9 +74,8 @@ public class ContactRepository implements IContactRepository{
 
     @Override
     public String DeleteContact(Integer id) {
-        ContactDTO contact= contacts.stream().
-                filter( c -> c.getId().equals(id)).
-                findFirst().orElseThrow(()-> new RuntimeException());
+        ContactDTO contact = contacts.stream().filter(c -> c.getId().equals(id)).findFirst()
+                .orElseThrow(() -> new RuntimeException());
         contacts.remove(contact);
         return "Deleted";
     }
