99cb4fb487abd834cb71034e692bab675e10263c
Viviana Diaz Velez <vdiazve@gmail.com>
2021-10-24 11:32:27 -0500

    integration test added

---
+++ src/test/java/co/edu/demo/integration/ContactControllerTest.java
@@ -0,0 +1,98 @@
+package co.edu.demo.integration;
+
+import co.edu.demo.model.dto.ContactDTO;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ContactControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    ObjectMapper mapper = new ObjectMapper();
+
+    @Test
+    public void TestGetContactSuccessful() throws Exception {
+        //Arrange
+        ContactDTO expectedContact = new ContactDTO(1, "Gabo", "123","Avenida siempre viva");
+
+        //Act
+        MvcResult response =this.mockMvc.perform(MockMvcRequestBuilders.get("/contact")
+                        .param("id",String.valueOf(1)))
+                .andReturn();
+
+        ContactDTO responseContact = mapper.readValue(response.getResponse().getContentAsString(), new TypeReference<>(){});
+
+        //Assert
+        Assertions.assertEquals(HttpStatus.OK.value(),response.getResponse().getStatus());
+        Assertions.assertEquals(expectedContact.getName(),responseContact.getName());
+    }
+    @Test
+    public void TestPostContactSuccessful() throws Exception {
+
+        //Arrange
+        ContactDTO expectedContact = new ContactDTO(4, "Juan", "234","Santuario");
+
+        String payload= mapper.writeValueAsString(expectedContact);
+
+        //Act
+        MvcResult response =this.mockMvc.perform(MockMvcRequestBuilders.post("/contact")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(payload))
+                .andReturn();
+
+        ContactDTO responseContact = mapper.readValue(response.getResponse().getContentAsString(), new TypeReference<>(){});
+
+        //Assert
+        Assertions.assertEquals(HttpStatus.CREATED.value(),response.getResponse().getStatus());
+        Assertions.assertEquals(expectedContact.getId(),responseContact.getId());
+    }
+
+    @Test
+    public void TestPutContactSuccessful() throws Exception {
+
+        //Arrange
+        ContactDTO expectedContact = new ContactDTO(3, "Juan Pablo", "234","Santuario");
+
+        String payload= mapper.writeValueAsString(expectedContact);
+
+        //Act
+        MvcResult response =this.mockMvc.perform(MockMvcRequestBuilders.put("/contact")
+                        .param("id",String.valueOf(3))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(payload))
+                .andReturn();
+
+        ContactDTO responseContact = mapper.readValue(response.getResponse().getContentAsString(), new TypeReference<>(){});
+
+        //Assert
+        Assertions.assertEquals(HttpStatus.OK.value(),response.getResponse().getStatus());
+        Assertions.assertEquals(expectedContact.getName(),responseContact.getName());
+    }
+    @Test
+    public void TestDeleteContactSuccessful() throws Exception {
+
+        //Act
+        MvcResult response =this.mockMvc.perform(MockMvcRequestBuilders.delete("/contact")
+                        .param("id",String.valueOf(1)))
+                .andReturn();
+        String actualResponse= response.getResponse().getContentAsString();
+
+        //Assert
+        Assertions.assertEquals(HttpStatus.OK.value(),response.getResponse().getStatus());
+        Assertions.assertEquals("Deleted",actualResponse);
+
+    }
+
+}
